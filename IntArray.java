f86512d5144b8527be55e92e1a0a3cb79759ee1d
alamodi <46985374+alamodi@users.noreply.github.com>
2019-01-28 13:12:03 +0800

    int array
    int array

---
+++ IntArray.java
@@ -0,0 +1,31 @@
+
+package activity.cc3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class IntArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int NM [] = new int [5];
+        int v = 0;
+        
+        
+       
+       for(int i = 0; i<NM.length; i++){
+           
+          System.out.println("enter number : ");
+       NM[i] = sc.nextInt();
+          
+       }
+       System.out.println("First to last: " +"\n"+Arrays.toString (NM));
+       System.out.println("last to first: ");
+       
+       for (int c = NM.length - 1 ;c>= 0; c--){
+           System.out.print((NM[c])+" ,");
+           
+    }
+       
+       
+    }
+}
